e24bdb37bfa93b7b8db2ad296d0dc1e2b777afd3
jackale <melon119@gmail.com>
2016-12-12 10:47:21 +0900

    [chap. 2] fix

--- 2nd/gate.py
+++ 2nd/gate.py
@@ -1,8 +1,14 @@
 import numpy as np
-def perceptron (w, b):
-    return lambda x: 1 if (b + np.sum(w*x)) > 0 else 0
+def perceptron (w1, w2, b):
+    w = np.array([w1, w2])
+    return lambda x1, x2: 1 if (b + np.sum(w*np.array([x1,x2]))) > 0 else 0
 
-AND  = perceptron(np.array([0.5, 0.5]), -0.7)
-OR   = perceptron(np.array([0.8, 0.8]), -0.7)
-NAND = perceptron(np.array([-0.5, -0.5]), 0.7)
-NOR   = perceptron(np.array([-0.8, -0.8]), 0.7)
+AND  = perceptron(0.5, 0.5, -0.7)
+OR   = perceptron(0.8, 0.8, -0.7)
+NAND = perceptron(-0.5, -0.5, 0.7)
+NOR  = perceptron(-0.8, -0.8, 0.7)
+
+def XOR (x1, x2):
+    s1 = OR(x1, x2)
+    s2 = NAND(x1, x2)
+    return AND(s1, s2)
--- 2nd/main.py
+++ 2nd/main.py
@@ -4,14 +4,13 @@ import gate
 table = ((0,0), (0,1), (1,0), (1,1))
 
 mes = '    ({0},{1}) => {2}'
-methods = ['AND', 'OR', 'NAND', 'NOR']
+methods = ['AND', 'OR', 'NAND', 'NOR', 'XOR']
 
 for method_name in methods:
     try:
         method = getattr(gate, method_name)
         print(method_name)
         for x1, x2 in table:
-            x = np.array([x1, x2])
-            print(mes.format(x1, x2, method(x)))
+            print(mes.format(x1, x2, method(x1, x2)))
     except AttributeError:
         print(method_name + ' is not defined.')
